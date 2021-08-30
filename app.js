6fbf46ceed888049c25b1ac92ffd7a9ec58e6c6a
rybarczykchris <rybarczyk.chris@gmail.com>
2021-08-30 13:06:00 +0200

    Adds basic auth

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const https = require('https');const shell = require('shelljs');
+const https = require('https');
+const shell = require('shelljs');
 const fs = require('fs');
+const basicAuth = require('express-basic-auth');
+const privateKey  = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+const app = require('express')();
+const {INTERCOM_USER, INTERCOM_PASS} = process.env
 
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
+app.use(basicAuth({
+    users: { [INTERCOM_USER]: `${INTERCOM_PASS}` },
+    challenge: true // <--- needed to actually show the login dialog!
+}));
 
-const port = 8008;
-
-https.createServer(options, function (req, res) {
+app.get('/', (req, res) => {
     res.writeHead(200);
     console.log("Received request at URL", req.url)
     shell.exec('./send_curl.sh')
-    res.end("Automation triggered");
-}).listen(port);
+    res.send("Automation triggered")
+})
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8008);
